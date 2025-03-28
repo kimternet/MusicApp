dd015668cba4a1a2dd15b6421a37d78448484076
kimternet <kimternet23@gmail.com>
2025-03-28 20:51:27 +0900

    user login 기능 추가

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -6,11 +6,13 @@ from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter, Depends
 from database import get_db
 from sqlalchemy.orm import Session
+from pydantic_schemas.user_login import UserLogin
+
 
 router = APIRouter()
 
 
-@router.post('/signup')
+@router.post('/signup', status_code=201)
 def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
 
@@ -25,4 +27,19 @@ def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     db.refresh(user_db)
 
     return user_db
+
+   
+@router.post('/login')
+def login_user(user: UserLogin, db:Session = Depends(get_db)):
+
+    user_db = db.query(User).filter(User.email == user.email).first()
     
+    if not user_db:
+        raise HTTPException(400, '존재하지 않는 이메일입니다.')
+    
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+
+    if not is_match:
+        raise HTTPException(400, '비밀번호가 일치하지 않습니다.')
+    
+    return user_db
