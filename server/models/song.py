cb3a1891ce941e81606ed7d70529bd821a54a2de
kimternet <kimternet23@gmail.com>
2025-04-01 14:41:50 +0900

    250401 클라우드, db 연동 완료

---
+++ server/models/song.py
@@ -0,0 +1,16 @@
+from models.base import Base
+from sqlalchemy import TEXT, VARCHAR, Column
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(TEXT, primary_key=True)
+    song_url = Column(TEXT)
+    thumbnail_url = Column(TEXT)
+    artist = Column(TEXT)
+    song_name = Column(VARCHAR(100))
+    hex_code = Column(VARCHAR(6))
+
+
+
